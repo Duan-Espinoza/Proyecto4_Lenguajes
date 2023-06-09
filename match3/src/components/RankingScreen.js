513a328fe19167daa044b761342771b2e9b0525c
EscalanteWizard <jordano.shordi@gmail.com>
2023-06-09 16:52:40 -0600

    GameScree y Ranking-Screen

---
+++ match3/src/components/RankingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RankingScreen = () => {
+  // Aquí puedes agregar la lógica y el contenido del componente RankingScreen
+
+  return (
+    <div>
+      <h1>Pantalla de Ranking</h1>
+      {/* Aquí puedes mostrar el ranking de puntajes */}
+    </div>
+  );
+};
+
+export default RankingScreen;
